a43bd35eee4058338b6003e26f63653c38b40724
piotr <piotr@debian9-pj.nsn-intra.net>
2019-03-13 16:34:22 +0100

    packaged task

---
+++ packaged_task.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include <future>
+using namespace std::chrono_literals;
+
+int addNumbers(int a, int b)
+{
+    std::cout << "Thinking..." << std::endl;
+    std::this_thread::sleep_for(1s);
+    std::cout << "I know!" << std::endl;
+    return a + b;
+}
+
+int main()
+{
+    
+    std::packaged_task<int (int, int)> myOperation(addNumbers);
+    
+    std::future<int> futureWithResultOfAddition = myOperation.get_future();
+
+    myOperation(1, 2);
+
+    std::this_thread::sleep_for(500ms);
+
+    std::cout << "Starting waiting for result.." << std::endl;
+
+    std::cout << "The result is " << futureWithResultOfAddition.get() << std::endl;
+
+    return 0;
+}
